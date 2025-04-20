dc8ea12c3f9e439cfa35d798f9075c002ef0d36e
TunDOTio <tuntun.pam3164@gmail.com>
2025-04-20 16:49:05 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ import pickle
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 
-#load model
-with open('kmeans_model.pkl', 'rb') as f:
-    loaded_model = pickle.load(f)
+#set the page config MUST BE FIRST
+st.set_page_config(page_title="K-Means Clustering", layout="centered")
 
 #set the title
 st.title("K-Means Clustering Visualizer by Peerawit Kubang")
 
-#set the page config
-st.set_page_config(page_title="K-Means Clustering", layout="centered")
+#load model
+with open('kmeans_model.pkl', 'rb') as f:
+    loaded_model = pickle.load(f)
 
 #load dataset
 X, _ = make_blobs(n_samples=300, centers=loaded_model.n_clusters, cluster_std=0.60, random_state=0)
